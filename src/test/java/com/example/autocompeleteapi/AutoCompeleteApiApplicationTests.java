f36318d4eeb3d5996ef784e3655f35431834e9b4
Rsthaa <sandipneupane65@gmail.com>
2022-09-27 03:24:15 -0500

    word autocomplete api

---
+++ src/test/java/com/example/autocompeleteapi/AutoCompeleteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autocompeleteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCompeleteApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
